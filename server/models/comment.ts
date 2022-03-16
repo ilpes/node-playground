aea18a9a53459560f210ffb5d99273f9a45521c5
Massimiliano Pesente <pesente.m@gmail.com>
2022-03-16 12:11:52 +0100

    core: create comment model

---
+++ server/models/comment.ts
@@ -0,0 +1,41 @@
+import Objection, {Model} from "objection";
+import User from "./user";
+import Upvote from "./upvote";
+import dayjs from "dayjs";
+
+class Comment extends  Model {
+
+    static tableName = 'comments';
+
+    static get relationMappings() {
+        return {
+            'user': {
+                relation: Model.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: 'comments.user_id',
+                    to: 'users.id'
+                }
+            },
+
+            'upvotes': {
+                relation: Model.HasManyRelation,
+                modelClass: Upvote,
+                join: {
+                    from: 'comments.id',
+                    to: 'upvotes.comment_id'
+                }
+            }
+        }
+    }
+
+    $formatJson(json: Objection.Pojo): Objection.Pojo {
+        json = super.$formatJson(json);
+        json.upvotes_count = json.upvotes.length;
+        json.readable_created_at = dayjs(json.created_at).fromNow();
+        delete json.upvotes;
+        return json;
+    }
+}
+
+export default Comment;
