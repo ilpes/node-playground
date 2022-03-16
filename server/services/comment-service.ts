db1d268907b3db918915dda593460f87d1ad07b8
Massimiliano Pesente <pesente.m@gmail.com>
2022-03-16 14:26:53 +0100

    core: add findById and upvote method to CommentService

--- server/services/comment-service.ts
+++ server/services/comment-service.ts
@@ -1,10 +1,23 @@
 import Comment from "../models/comment";
+import Upvote from "../models/upvote";
 
 class CommentService {
     async save(data: object): Promise<Comment | undefined> {
         return Comment.query()
             .insert(data)
             .withGraphFetched('[user, upvotes]');
+    }
+
+    async findById(id: number): Promise<Comment | undefined> {
+        return Comment.query()
+            .select('*')
+            .withGraphFetched('[user, upvotes]')
+            .findById(id);
+    }
+
+    async upvote(data: object): Promise<Upvote | undefined> {
+        return Upvote.query()
+            .insert(data)
     }
 }
 
