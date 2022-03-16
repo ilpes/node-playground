387d9f04729f420b051ecfc667714c3c388b1581
Massimiliano Pesente <pesente.m@gmail.com>
2022-03-16 12:11:43 +0100

    core: create post model

---
+++ server/models/post.ts
@@ -0,0 +1,21 @@
+import Objection, {Model} from "objection";
+import Comment from "./comment";
+
+class Post extends  Model {
+    static tableName = 'posts';
+
+    static get relationMappings() {
+        return {
+            'comments': {
+                relation: Model.HasManyRelation,
+                modelClass: Comment,
+                join: {
+                    from: 'posts.id',
+                    to: 'comments.post_id'
+                }
+            }
+        }
+    }
+}
+
+export default Post;
