14add4042a647449e2dcaec4b3170a5e1687a608
Massimiliano Pesente <pesente.m@gmail.com>
2022-03-16 11:21:37 +0100

    db: add users seed

---
+++ server/database/seeds/add_users.ts
@@ -0,0 +1,15 @@
+import { Knex } from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex("users").del();
+
+    // Inserts seed entries
+    await knex("users").insert([
+        { username: "Doris Stone", avatar: "https://randomuser.me/api/portraits/women/62.jpg" },
+        { username: "Brenda Cox", avatar: "https://randomuser.me/api/portraits/women/9.jpg" },
+        { username: "Elva Murphy", avatar: "https://randomuser.me/api/portraits/women/50.jpg" },
+        { username: "Randy Clements", avatar: "https://randomuser.me/api/portraits/men/3.jpg" },
+        { username: "Jody Green", avatar: "https://randomuser.me/api/portraits/men/80.jpg" },
+    ]);
+};
