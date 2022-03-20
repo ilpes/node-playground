62eddb8f37c8db4804852511bb5ac36e30f97a57
Massimiliano Pesente <pesente.m@gmail.com>
2022-03-20 10:51:54 +0100

    model: add comment relationship to upvote

--- server/models/upvote.ts
+++ server/models/upvote.ts
@@ -1,7 +1,23 @@
 import Objection, {Model} from "objection";
+import Comment from "./comment";
 
 class Upvote extends Model {
     static tableName = 'upvotes';
+
+    comment?: Comment;
+
+    static get relationMappings() {
+        return {
+            'comment': {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Comment,
+                join: {
+                    from: 'upvotes.comment_id',
+                    to: 'comments.id'
+                }
+            }
+        }
+    }
 }
 
 export default Upvote;
